0f0f689a951bf0249b0a3c3a4a5d09739d9538dd
Guilain De Meyer <de.meyer.guilain@gmail.com>
2024-10-07 10:45:09 +0200

    Quoi de neuf Doc initialisation

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Quoi de neuf Doc ?</title>
+</head>
+<body>
+    
+    <header>
+        <img src="/src/assets/images/" alt="">
+    </header>
+
+    <script src="javascript/script.js"></script>
+</body>
+</html>
